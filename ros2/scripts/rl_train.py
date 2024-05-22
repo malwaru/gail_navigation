65a68cc060b49f7b7b3abe64a8b35c3f36eade6e
Malika Navaratna <malikawarunamal@gmail.com>
2024-05-22 09:36:44 +0000

    added learning rate scheduler

--- ros2/scripts/rl_train.py
+++ ros2/scripts/rl_train.py
@@ -10,12 +10,31 @@ from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy, CnnPolicy
 from imitation.data.wrappers import RolloutInfoWrapper
 from stable_baselines3.common.evaluation import evaluate_policy
+from typing import Callable
+
 
 import rclpy
 import numpy as np
 
 
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
 
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
 
 def train_gail(rollouts,batch_size,no_envs=1):
     '''
@@ -44,7 +63,7 @@ def train_gail(rollouts,batch_size,no_envs=1):
         policy=MlpPolicy,
         batch_size=64, # Mini batch size
         ent_coef=0.0,  # Entropy coefficient for the loss calculation
-        learning_rate=0.0004,
+        learning_rate=linear_schedule(0.0009),
         gamma=0.95, # Discount factor
         n_epochs=5, # Number of epochs when optimizing the surrogate objective
         seed=SEED,
@@ -64,6 +83,7 @@ def train_gail(rollouts,batch_size,no_envs=1):
         gen_algo=learner,
         reward_net=reward_net,
         allow_variable_horizon=False,
+        
     )
 
     env.seed(SEED)
